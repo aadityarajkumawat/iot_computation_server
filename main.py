423a3a3ef301995a3951cc5fe8c5d91b500dd12a
Aditya Raj Kumawat <arkumawat78@gmail.com>
2022-10-08 20:27:06 +0530

    cool

--- main.py
+++ main.py
@@ -4,6 +4,21 @@ from validators.pulse_data_validator import pulse_data_validator
 
 app = Flask(__name__)
 
+@app.route('/')
+def home():
+    return """
+    <div>
+        <h1>PR1101: Health Insight Monitor</h1>
+        <p>This is the API used by our IoT enabled health insight monitor</p>
+        <p>Endpoint</p>
+        <ul>
+            <li>GET: /</li>
+            <li>POST: /heart_pulse_data</li>
+            <li>GET: /get_insights</li>
+        </ul>
+    </div>
+    """
+
 @app.route("/heart_pulse_data")
 def heart_pulse_data():
     response_type = {"data": {}, "error": None}
@@ -15,3 +30,7 @@ def heart_pulse_data():
         return response_type
 
     return body
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host='0.0.0.0', port=3000)
